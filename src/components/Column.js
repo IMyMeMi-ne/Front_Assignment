4bf5c107845ebcbbeafcac7038582ececf3335af
Namhyun <jenple9709@gmail.com>
2024-07-30 17:34:01 +0900

    refactor: Column 컴포넌트 분리

---
+++ src/components/Column.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+
+export default function Column({
+  droppableId,
+  items,
+  getListStyle,
+  getItemStyle,
+}) {
+  return (
+    <Droppable droppableId={droppableId}>
+      {(provided, snapshot) => (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}
+        >
+          {items.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}
+                >
+                  {item.content}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,88 +1,84 @@
-import React, { useState, useCallback } from "react";
-import ReactDOM from "react-dom";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
-function App() {
-  const getItems = (count) =>
-    Array.from({ length: count }, (v, k) => k).map((k) => ({
-      id: `item-${k}`,
-      content: `item ${k}`,
-    }));
-
-  const [items, setItems] = useState(getItems(10));
-
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-  };
-
-  const onDragEnd = useCallback(
-    (result) => {
-      if (!result.destination) {
-        return;
-      }
-
-      const newItems = reorder(
-        items,
-        result.source.index,
-        result.destination.index
-      );
-
-      setItems(newItems);
-    },
-    [items]
-  );
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided, snapshot) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            style={getListStyle(snapshot.isDraggingOver)}
-          >
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided, snapshot) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={getItemStyle(
-                      snapshot.isDragging,
-                      provided.draggableProps.style
-                    )}
-                  >
-                    {item.content}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
-}
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Column from './components/Column';
+import useDragAndDrop from './hooks/useDragAndDrop';
+import './index.css';
 
 const GRID = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-  userSelect: "none",
+  userSelect: 'none',
   padding: GRID * 2,
   margin: `0 0 ${GRID}px 0`,
-  background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? 'lightgreen' : 'grey',
   ...draggableStyle,
 });
 
 const getListStyle = (isDraggingOver) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
   padding: GRID,
   width: 250,
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const getItems = (count, col) => {
+  return Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `${col}-${k}`,
+    content: `item ${k}`,
+  }));
+};
+
+const App = () => {
+  const [firstItems, setFirstItems] = useState(getItems(10, 'first'));
+  const [secondItems, setSecondItems] = useState(getItems(10, 'second'));
+  const [thirdItems, setThirdItems] = useState(getItems(10, 'third'));
+  const [fourthItems, setFourthItems] = useState(getItems(10, 'fourth'));
+
+  const { onDragEnd } = useDragAndDrop(
+    firstItems,
+    setFirstItems,
+    secondItems,
+    setSecondItems,
+    thirdItems,
+    setThirdItems,
+    fourthItems,
+    setFourthItems
+  );
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="flex">
+        <Column
+          droppableId="droppable1"
+          items={firstItems}
+          setItems={setFirstItems}
+          getListStyle={getListStyle}
+          getItemStyle={getItemStyle}
+        />
+        <Column
+          droppableId="droppable2"
+          items={secondItems}
+          setItems={setSecondItems}
+          getListStyle={getListStyle}
+          getItemStyle={getItemStyle}
+        />
+        <Column
+          droppableId="droppable3"
+          items={thirdItems}
+          setItems={setThirdItems}
+          getListStyle={getListStyle}
+          getItemStyle={getItemStyle}
+        />
+        <Column
+          droppableId="droppable4"
+          items={fourthItems}
+          setItems={setFourthItems}
+          getListStyle={getListStyle}
+          getItemStyle={getItemStyle}
+        />
+      </div>
+    </DragDropContext>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
