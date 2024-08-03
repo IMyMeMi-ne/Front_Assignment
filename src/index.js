107f2647254ca57c6d200c0ac2eb670302ce96ea
Namhyun <jenple9709@gmail.com>
2024-08-03 20:44:36 +0900

    design: bg색 변경

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const getItemStyle = (
 });
 
 const getListStyle = (isDraggingOver) => ({
-  backgroundColor: isDraggingOver ? '#3d3d3d' : 'lightgrey',
+  backgroundColor: isDraggingOver ? '#3d3d3d' : 'white',
   padding: GRID,
   width: 250,
 });
@@ -76,7 +76,7 @@ const App = () => {
     );
 
   return (
-    <div className="flex">
+    <div className="flex flex-col lg:flex-row">
       <DragDropContext
         onDragStart={onDragStart}
         onDragEnd={onDragEnd}
@@ -114,18 +114,22 @@ const App = () => {
           />
         </div>
       </DragDropContext>
-      <div className="flex flex-col ml-7 mr-7 w-full border-2 border-gray-300 bg-[#3d3d3d] p-4 text-white">
-        <div className="flex justify-center text-4xl font-bold">
+      <div className="flex flex-col ml-7 mr-7 w-full border-2 border-zinc-800 bg-[#3d3d3d] p-4 text-white">
+        <div className="flex justify-center text-4xl font-bold bg-[#3d3d3d]">
           드래그 앤 드롭
         </div>
         <hr className="w-full my-4 border-t-2 border-gray-400" />
-        <div className="font-semibold text-xl mt-10">
+        <div className="font-semibold text-xl mt-10 bg-[#3d3d3d]">
           4개의 컬럼에서 자유롭게 아이템을 드래그 앤 드롭 해보세요!
         </div>
-        <div className="mt-4 font-semibold">제약 조건</div>
-        <span>1. 1번째 컬럼에서 3번째 컬럼으로는 이동이 불가능합니다.</span>
-        <span>2. 짝수 아이템의 위로 짝수 아이템을 놓을 수 없습니다.</span>
-        <div className="mt-auto">
+        <div className="mt-4 font-semibold bg-[#3d3d3d]">제약 조건</div>
+        <span className="bg-[#3d3d3d]">
+          1. 1번째 컬럼에서 3번째 컬럼으로는 이동이 불가능합니다.
+        </span>
+        <span className="bg-[#3d3d3d]">
+          2. 짝수 아이템의 위로 짝수 아이템을 놓을 수 없습니다.
+        </span>
+        <div className="mt-auto bg-[#3d3d3d]">
           <button
             className="w-24 h-16 bg-gray-200 relative p-2 text-[#3d3d3d] text-2xl rounded-xl"
             onClick={reset}
